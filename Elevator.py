bc6368f1f3be5b9aabd88b59df3c2ef7b1589809
Grace0 <gracekwak25@gmail.com>
2019-01-25 23:50:49 -0800

    Add elev constants

--- Elevator.py
+++ Elevator.py
@@ -7,7 +7,7 @@
 
 import numpy
 from matplotlib import pylab
-import slycot 
+import slycot
 import controls
 import control_loop
 import UnitTest.profile as prof
@@ -16,14 +16,14 @@ import UnitTest.profile as prof
 controller_time_step = .005 #10 milliseconds
 sim_time_step = .000001
 
-#time in seconds
+#sim runtime in seconds
 total_time = 10
 
 ##Elevator is a daughter of the ControlLoop superclass
 class Elevator(control_loop.ControlLoop):
-    
-    def __init__(self, name = "Intake") :
-        
+
+    def __init__(self, name = "Elevator") :
+        # Stall Torque in N m
         self.stall_torque = 0.71 * 2
         # Stall Current in Amps
         self.stall_current = 134 * 2
@@ -39,18 +39,17 @@ class Elevator(control_loop.ControlLoop):
         # Torque constant
         self.Kt = self.stall_torque / self.stall_current
         # Gear ratio
-        self.G = 20.0
+        self.G = 25.0
         #radius in meters
-        self.r = 0.0381
-        #mass of the elevator carriage, the arm and the cube is 19 pounds (divide by 2.2 to get kg)
-        self.mass = 19.0/2.2
-         
-        self.max_vel = (self.free_speed/self.G) * (2.0 * 3.1415 * self.r)
+        self.r = 0.1 # 4 in
+        #mass of the elevator carriage, intake, and ball/hatch is pounds (divide by 2.2 to get kg)
+        self.mass = 19.0/2.2 #TODO: find out
 
-        
+        self.max_vel = (self.free_speed/self.G) * (2.0 * numpy.py * self.r)
+
         C1 = self.Kt * self.G * self.G / (self.Kv * self.resistance * self.r * self.r * self.mass)
         C2 = self.G * self.Kt / (self.resistance * self.r * self.mass)
-        
+
         self.A_continuous = numpy.matrix(
         [[0, 1],
          [0, -C1]])
@@ -59,51 +58,51 @@ class Elevator(control_loop.ControlLoop):
         self.B_continuous = numpy.matrix(
                 [[0],
                 [C2]])
-    
+
         self.C = numpy.matrix([[1, 0]])
         self.D = numpy.matrix([[0]])
-        
-        
+
+
         self.A, self.B = self.ContinuousToDiscrete(
         self.A_continuous, self.B_continuous, controller_time_step)
-        
-        
-        #LQR
-        q_pos = .65#.55#0.35
-        q_vel = .9#2.5#1.0
+
+
+        #tolerance of state error
+        q_pos = .65
+        q_vel = .9
         self.Q = numpy.matrix([[(1.0 / (q_pos ** 2.0)), 0.0],
                            [0.0, (1.0 / (q_vel ** 2.0))]])
 
-        self.R = numpy.matrix([[(1.0 / (12.0 ** 2.0))]])
+        self.R = numpy.matrix([[(1.0 / (12.0 ** 2.0))]]) #penalty for large u
         self.K = controls.dlqr(self.A, self.B, self.Q, self.R)
-        
+
         self.U_max = numpy.matrix([[12.0]])
         self.U_min = numpy.matrix([[-12.0]])
- 
+
         self.InitializeState()
-        
+
         print(self.K)
-        
-        
+
+
     def dlqr(A, B, Q, R):
       """Solves for the optimal lqr controller.
-    
+
         x(n+1) = A * x(n) + B * u(n)
         J = sum(0, inf, x.T * Q * x + u.T * R * u)
       """
-    
+
       # P = (A.T * P * A) - (A.T * P * B * numpy.linalg.inv(R + B.T * P *B) * (A.T * P.T * B).T + Q
       P, rcond, w, S, T = slycot.sb02od(n=A.shape[0],m=B.shape[1],A=A,B=B,Q=Q,R=R,dico='D')
-    
+
       F = numpy.linalg.inv(R + B.T * P *B) * B.T * P * A
       return F
-        
-        
+
+
 
 class IntegralElevator(Elevator):
-    
+
     def __init__(self, name = "IntegralElevator"):
-        
+
         super(IntegralElevator, self).__init__(name=name)
 
         self.A_continuous_unaugmented = self.A_continuous
@@ -122,8 +121,8 @@ class IntegralElevator(Elevator):
 
         self.A, self.B = self.ContinuousToDiscrete(
         self.A_continuous, self.B_continuous, controller_time_step)
-        
-       
+
+
         q_pos = 0.25
         q_vel = 0.10
         q_voltage = 4.0
@@ -138,16 +137,16 @@ class IntegralElevator(Elevator):
         self.K = numpy.matrix(numpy.zeros((1, 3)))
         self.K[0, 0:2] = self.K_unaugmented
         self.K[0, 2] = 1
-              
+
         self.X = numpy.zeros((self.A.shape[0], 1))
         self.Y = self.C * self.X
         self.X_hat = numpy.zeros((self.A.shape[0], 1))
-        
+
         self.InitializeState()
 
 
 class Simulator(object):
-    
+
     def __init__(self):
         self.t = []
         self.x = []
@@ -156,77 +155,73 @@ class Simulator(object):
         self.x_hat = []
         self.u = []
         self.offset = []
-        
+
     def simulate(self, elevator, goal_pos, goal_vel,
              controller_elevator,
              t_time):
-        
+
         if controller_elevator is None:
             controller_elevator = elevator
-       
+
         vbat = 12.0
-        
+
         initial_t = 0
-        
+
         time = initial_t
-            
-        profile_index = 0       
+
+        profile_index = 0
 
         while(time < t_time):
-            
+
             goal_data = [goal_pos[profile_index], goal_vel[profile_index]]
-         
+
             velocity_goal = goal_vel[profile_index]
-            
+
             U = controller_elevator.K * (goal_data - elevator.X) + ((velocity_goal/elevator.max_vel) * vbat)
-            
+
             U[0, 0] = numpy.clip(U[0, 0], -vbat, vbat)
             self.x.append(elevator.X[0, 0])
-            
+
             if self.v:
                 last_v = self.v[-1]
             else:
                 last_v = 0
-                       
+
             self.x_hat.append(elevator.X_hat[0,0])
             self.v.append(elevator.X[1, 0])
             self.a.append((self.v[-1] - last_v) / controller_time_step)
-          
+
             elevator.Update(U)
-          
+
             self.t.append(time)
             self.u.append(U[0, 0])
-            
+
             if (profile_index < len(goal_pos) - 1):
                 profile_index += 1
-            
+
             time += controller_time_step
-            
+
     def Plot(self):
-        
+
         pylab.subplot(3, 1, 1)
         pylab.plot(self.t, self.x, label='x')
         pylab.plot(self.t, self.x_hat, label='x_hat')
         pylab.legend()
-    
+
         pylab.subplot(3, 1, 2)
         pylab.plot(self.t, self.u, label='u')
        # pylab.plot(self.t, self.offset, label='voltage_offset')
         pylab.legend()
-    
+
         pylab.subplot(3, 1, 3)
         pylab.plot(self.t, self.a, label='a')
         pylab.legend()
-    
-        pylab.show()
-        
-        
 
-    
-print("oh god")
-    
+        pylab.show()
+
+
 simulator = Simulator()
-    
+
 elevator = Elevator()
 elevator_controller = IntegralElevator()
 
@@ -235,7 +230,7 @@ vals = profiler.generateProfile(1)
 
 pos = vals[0]
 vels = vals[1]
-    
+
 R = numpy.matrix([[1.2], [0.0]])
 simulator.simulate(elevator, pos, vels, controller_elevator=None, t_time = total_time)
 
